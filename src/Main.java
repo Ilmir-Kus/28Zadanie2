7bbb2a6c5bd160de9f1527828272e439f4a247bf
Neto test <ilmir.kusmaev03@mail.ru>
2023-02-04 09:46:34 +0400

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int account = 200; //Начальная сумма
-        int deposit = 900; // Пополнение
+        int deposit = 1100; // Пополнение
         int bonus;
         if (deposit > 1000) {
             bonus = deposit / 100;
             System.out.println("Кол-во бонусных рублей:" + bonus);
-        } else bonus = 0;
-        {
+        } else {
+            bonus = 0;
             System.out.println("Кол-во бонусных рублей:" + bonus);
         }
     }
